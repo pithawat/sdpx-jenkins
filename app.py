991812db575b56d64346262699d71f002099785b
pithawat <linkpittawat@gmail.com>
2025-09-20 21:20:58 +0700

    api and jenkinsfile

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Example route
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to my Flask API!"})
+
+# Example GET route
+@app.route('/getcode', methods=['GET'])
+def hello():
+    return jsonify({"message": "hello"})
+
+@app.route('/plus/<int:x>/<int:y>', methods=['GET'])
+def plus(x,y):
+    result = x+y
+    return jsonify({
+        "x":x,
+        "y":y,
+        "sum":result
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
