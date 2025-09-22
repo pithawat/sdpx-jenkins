fd33266963bce934021f837c567ddedfacbf3f2c
pithawat <linkpittawat@gmail.com>
2025-09-22 19:28:04 +0700

    .

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
 # Example GET route
 @app.route('/getcode', methods=['GET'])
 def hello():
-    return jsonify({"message": "hello"})
+    return jsonify({"message": "kuy few"})
 
 @app.route('/plus/<int:x>/<int:y>', methods=['GET'])
 def plus(x,y):
